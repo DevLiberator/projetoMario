b36175dbd6e26a4564aa93f32cff7057f1710076
DevLiberator <tectecnico535@gmail.com>
2025-11-23 12:48:20 -0300

    colocando imagemda cabeça do Mario e Alteração no @media

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const Form = document.querySelector('.form-contato');
 const maskForm = document.querySelector('.mask-form');
 
 function callForm() {
-        Form.style.left = "35%";
-        maskForm.style.visibility = "visible";
-    };
+    Form.style.left = "35%";
+    maskForm.style.visibility = "visible";
+};
 
 function mask() {
-        Form.style.left = "-100%";        
-        maskForm.style.visibility = "hidden";
-    };
+    Form.style.left = "-100%";
+    maskForm.style.visibility = "hidden";
+};
